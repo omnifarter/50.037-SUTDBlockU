ff96213d0d09ef8e8e2869b5f041be287028f145
Gabriel <gabrielphang@gmail.com>
2022-04-14 19:37:28 +0800

    add metamask detector

--- client/src/helpers/useMetaMask.tsx
+++ client/src/helpers/useMetaMask.tsx
@@ -4,6 +4,7 @@ import { UNI_TOKEN_ADDRESS } from "./constants";
 //@ts-ignore
 import UniToken from "../abis/UniToken.json";
 import { useNavigate } from "react-router-dom";
+import detectEthereumProvider from '@metamask/detect-provider';
 
 type ContextData = {
   metaAddress?: string;
@@ -14,9 +15,7 @@ export const Context = React.createContext<ContextData>({});
 const useMetaMask = () => {
   const [metaAddress, setMetaAddress] = useState("");
   //@ts-ignore
-  const [provider, _setProvider] = useState(
-    //@ts-ignore
-    new ethers.providers.Web3Provider(window.ethereum)
+  const [provider, _setProvider] = useState<ethers.providers.Web3Provider>(
   );
 
   const [uniTokenContract, setUniTokenContract] = useState<ethers.Contract>();
@@ -24,16 +23,19 @@ const useMetaMask = () => {
   // A Web3Provider wraps a standard Web3 provider, which is
   // what MetaMask injects as window.ethereum into each page
   const initMetaMask = async () => {
-    await provider.send("eth_requestAccounts", []);
-    // MetaMask requires requesting permission to connect users accounts
-
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...
-
-    const signer = provider.getSigner();
-    setMetaAddress(await signer.getAddress());
-    await createUniTokenContract(signer);
+    const provider = await detectEthereumProvider({mustBeMetaMask:true}) as ethers.providers.Web3Provider
+    if (provider){
+      await provider.send("eth_requestAccounts", []);
+      // MetaMask requires requesting permission to connect users accounts
+  
+      // The MetaMask plugin also allows signing transactions to
+      // send ether and pay to change state within the blockchain.
+      // For this, you need the account signer...
+  
+      const signer = provider.getSigner();
+      setMetaAddress(await signer.getAddress());
+      await createUniTokenContract(signer);
+    }
   };
 
   const createUniTokenContract = async (signer: any) => {
